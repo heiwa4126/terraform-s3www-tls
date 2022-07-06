dce3e8440573b3f6968e0ad421760999d85fee1f
heiwa4126 <heiwa4126@gmail.com>
2022-07-06 10:49:50 +0900

    initial (empty project)

---
+++ main.tf
@@ -0,0 +1,31 @@
+variable "prefix" {
+  default = "s3www-"
+}
+variable "aws_region" {
+  default = "ap-northeast-3"
+}
+variable "author_mail" {
+  default = "foo@example.com"
+}
+locals {
+  prefix = var.prefix # just as macro
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = {
+      mail        = var.author_mail
+      provided_by = "Terraform"
+    }
+  }
+}
